13d7c73a40dffd8de2aa2c1f1d4e2d67ea18a1f4
Yunfei Si <silenceocean@live.com>
2015-06-18 16:26:07 +0800

    finish 066,067,068

---
+++ 067/InorderTraversal.java
@@ -0,0 +1,28 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+        
+        if(root!=null) {
+            inorderTraversal(root.left);
+            list.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return list;
+    }
+}
---
+++ 068/PostorderTraversal.java
@@ -0,0 +1,27 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    /**
+     * @param root: The root of binary tree.
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        // write your code here
+        if(root!=null) {
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            list.add(root.val);
+        }
+        return list;
+    }
+}
