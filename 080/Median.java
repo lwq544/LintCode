ad663fdf56f035ba612fd2f677344b8d1f1fcbd0
Yunfei Si <silenceocean@live.com>
2015-06-18 16:09:40 +0800

    finish 080

---
+++ 080/Median.java
@@ -0,0 +1,16 @@
+public class Solution {
+    /**
+     * @param nums: A list of integers.
+     * @return: An integer denotes the middle number of the array.
+     */
+    public int median(int[] nums) {
+        // write your code here
+        Arrays.sort(nums);
+        if (nums.length % 2 == 0) {
+            return nums[nums.length / 2 - 1];
+        } else {
+            return nums[nums.length / 2];
+        }
+    }
+}
+
