7303a1fefa7f1af83f865c3ee8fef3e977b6afb4
Yunfei Si <silenceocean@live.com>
2016-04-01 09:53:18 +0800

    update

--- 002 尾部的零/TrailingZeros.java
+++ 002 尾部的零/TrailingZeros.java
@@ -6,7 +6,7 @@ class Solution {
     public long trailingZeros(long n) {
         // write your code here
         long count = 0;
-        while (n/5 > 0) {
+        while (n / 5 > 0) { // 5*2=10，显然2的个数比5多，所以只要算出5的个数
             n = n / 5;
             count += n;
         }
