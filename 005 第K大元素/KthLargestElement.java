a0e92b57fd1fc4970332d71e8c4735a352ad44b6
Yunfei Si <silenceocean@live.com>
2016-04-02 12:48:34 +0800

    udpate 005

--- 005 第K大元素/KthLargestElement.java
+++ 005 第K大元素/KthLargestElement.java
@@ -1,23 +1,42 @@
-class Solution {
-    //param k : description of k
-    //param numbers : array of numbers
-    //return: description of return
-    public int kthLargestElement(int k, ArrayList<Integer> numbers) {
-        // write your code here
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Silocean on 2016-04-02.
+ */
+public class KthLargestElement {
+    public static void main(String[] args) {
+        int[] nums = {9, 3, 2, 4, 8};
+        System.out.println(kthLargestElement2(3, nums));
+    }
+
+    /**
+     * 时间复杂度为O(K*N)，不适用数组太大的情况。
+     *
+     * @param k
+     * @param nums
+     * @return
+     */
+    public static int kthLargestElement(int k, int[] nums) {
         int max = Integer.MIN_VALUE;
-        int index = 0;
-        while (k>0) {
+        while (k > 0) {
             max = Integer.MIN_VALUE;
-            for (int i=0; i<numbers.size(); i++) {
-                if(numbers.get(i) > max) {
-                    max = numbers.get(i);
+            int index = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] > max) {
+                    max = nums[i];
                     index = i;
                 }
             }
-            numbers.remove(index);
+            nums[index] = Integer.MIN_VALUE;
             k--;
-            //System.out.println(numbers);
         }
         return max;
     }
-};
+
+    public static int kthLargestElement2(int k, int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+}
