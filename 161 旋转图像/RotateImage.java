d7b44949220c3e2b3851cfb89322c07677353a67
Yunfei Si <silenceocean@live.com>
2016-05-04 17:48:54 +0800

    finish 161

---
+++ 161 旋转图像/RotateImage.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Silocean on 2016-05-04.
+ */
+public class RotateImage {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * @param matrix: A list of lists of integers
+     * @return: Void
+     */
+    public static void rotate(int[][] matrix) {
+        // write your code here
+        int length = matrix.length;
+        for (int i = 0; i < length / 2; i++) {
+            for (int j = 0; j < (length + 1) / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[length - j - 1][i];
+                matrix[length - j - 1][i] = matrix[length - i - 1][length - j - 1];
+                matrix[length - i - 1][length - j - 1] = matrix[j][length - i - 1];
+                matrix[j][length - i - 1] = temp;
+            }
+        }
+    }
+}
