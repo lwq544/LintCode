11f3fee65fc62235db99a55e4a22fbd69365ec87
Yunfei Si <silenceocean@live.com>
2016-05-16 21:24:30 +0800

    finish 070

---
+++ 070 二叉树的层次遍历II/LevelOrderBottom.java
@@ -0,0 +1,58 @@
+package LevelOrderBottom;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by Silocean on 2016-05-16.
+ */
+public class LevelOrderBottom {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        ArrayList<ArrayList<Integer>> list = levelOrderBottom(root);
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).size(); j++) {
+                System.out.print(list.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+        // write your code here
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            result.add(0, level);
+        }
+        return result;
+    }
+}
+
+class TreeNode {
+    public int val;
+    public TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
