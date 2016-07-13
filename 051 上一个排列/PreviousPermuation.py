cbf3fee3b18b2a04562cb000540e8c48179c9c57
Yunfei Si <silenceocean@live.com>
2016-07-13 17:20:55 +0800

    finish 051

---
+++ 051 上一个排列/PreviousPermuation.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param num :  a list of integer
+    # @return : a list of integer
+    def previousPermuation(self, num):
+        # write your code here
+        for i in xrange(len(num) - 1, 0, -1):
+            if num[i] < num[i - 1]:
+                tmp = num[i - 1]
+                b = sorted(num[i:], reverse=True)
+                for x in b:
+                    if x < tmp:
+                        num[i - 1] = x
+                        b.remove(x)
+                        b.append(tmp)
+                        break
+                a = num[:i]
+                b = sorted(b, reverse=True)
+                for x in b:
+                    a.append(x)
+                return a
+        return num[::-1]
